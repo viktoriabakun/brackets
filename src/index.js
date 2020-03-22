201feb34f7715ad790a4f87da979d2c5cc7cb2c2
viktoryia bakun <viktoriabakun00@gmail.com>
2020-03-22 20:20:14 +0300

    feat: implement a function for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+
+  for (let i = 0; i < bracketsConfig.length;) {
+      let brTogether = bracketsConfig[i].join('');
+
+      if (str.includes(brTogether)) {
+          str = str.replace(brTogether, '');
+          i= 0;
+      } else {
+          i++;
+      }
+  }
+
+  return str.length ? false : true;
 }
